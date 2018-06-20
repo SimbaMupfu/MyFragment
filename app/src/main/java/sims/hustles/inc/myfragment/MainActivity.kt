fe40000da5c6bafc3da1fa94250448d066cdaa90
SimsHustles <scmupfururirwa@gmail.com>
2018-06-20 14:32:20 +0200

    Initial commit

---
+++ app/src/main/java/sims/hustles/inc/myfragment/MainActivity.kt
@@ -0,0 +1,43 @@
+package sims.hustles.inc.myfragment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    var isFragmentOneLoaded = true
+    val manager = supportFragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val change = findViewById<Button>(R.id.btn_change)
+        showFragmentOne()
+        change.setOnClickListener({
+            if(isFragmentOneLoaded)
+                showFragmentTwo()
+            else
+                showFragmentOne()
+        })
+    }
+
+    fun showFragmentOne(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentOne()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = true
+    }
+
+    fun showFragmentTwo(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentTwo()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = false
+    }
+}
